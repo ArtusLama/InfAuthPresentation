7cb83a23bbc2a652858e346e17b99929cce53d2b
Artus <artuslama@gmail.com>
2025-06-21 13:19:19 +0200

    🎉 Initial Commit

---
+++ app/components/LanguageSwitchButton/LanguageSwitchButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <UiDropdownMenu>
+        <UiDropdownMenuTrigger as-child>
+            <UiButton
+                variant="outline"
+                class="flex items-center justify-center"
+            >
+                <Icon name="lucide:globe" />
+                <span class="-mt-0.5">
+                    {{ locale.toUpperCase() }}
+                </span>
+                <span class="sr-only">Switch language</span>
+            </UiButton>
+        </UiDropdownMenuTrigger>
+        <UiDropdownMenuContent>
+            <UiDropdownMenuItem
+                v-for="loc in locales"
+                :key="loc.code"
+            >
+                <SwitchLocalePathLink
+                    :locale="loc.code"
+                    class="flex items-center gap-2"
+                >
+                    <LanguageSwitchButtonFlag :locale-code="loc.code" />
+                    <p>{{ loc.name || loc.code.toUpperCase() }}</p>
+                </SwitchLocalePathLink>
+            </UiDropdownMenuItem>
+        </UiDropdownMenuContent>
+    </UiDropdownMenu>
+</template>
+
+<script lang="ts" setup>
+const { locales, locale } = useI18n()
+</script>
